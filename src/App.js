c9560471d6b2143cecd906cd6d9706e4ca43ec22
florian godin <florian@misterflog>
2018-02-01 12:25:33 +0100

    implement clicked user datas

--- src/App.js
+++ src/App.js
@@ -12,11 +12,16 @@ import UserInfo from './components/UserInfo';
 class App extends Component {
   state = {
     profiles: [],
+    check: false,
+    temp_picture: null,
+    temp_name: "",
+    temp_points: 0,
   };
 
   componentWillMount() {
     this.setState({
       profiles: PROFILES,
+      check: false,
     });
   }
 
@@ -25,13 +30,17 @@ class App extends Component {
       <div>
         <Ranking
           profiles={this.state.profiles}
-          onSelectProfile={() => {}}
+          onSelectProfile={(user) => {this.setState ({check: true,
+          temp_picture: user.picture,
+          temp_name: user.name,
+          temp_points: user.points,
+    });}}
         />
-        <Modal show={false} onHide={() => {}}>
+        <Modal show={this.state.check}   onHide={() => {this.setState ({check: false});}} >
           <UserInfo
-            picture={null}
-            name=""
-            points={0}
+            picture={this.state.temp_picture}
+            name={this.state.temp_name}
+            points={this.state.temp_points}
           />
         </Modal>
       </div>
